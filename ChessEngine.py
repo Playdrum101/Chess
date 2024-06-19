f92953297206d52328fc6d7f678da09b1654a0f2
Sourabh Dhar <sourabhdhar2004@gmail.com>
2024-06-19 19:14:57 +0530

    Update #1
    not working because something to with valid moves

--- ChessEngine.py
+++ ChessEngine.py
@@ -13,11 +13,50 @@ class GameState():
             ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
         self.whitetoMove= True
         self.moveLog = []
+
+    def undoMove(self):
+         if len(self.moveLog)!=0:
+              move = self.moveLog.pop()
+              self.board[move.startRow][move.startCol] = move.pieceMoved
+              self.board[move.endRow][move.endCol] = move.pieceCaptured
+              self.whitetoMove = not self.whitetoMove
+              
+
     def makeMove(self,move):
             self.board[move.startRow][move.startCol] = "--"
             self.board[move.endRow][move.endCol] = move.pieceMoved
             self.moveLog.append(move)
             self.whitetoMove = not self.whitetoMove
+
+    def getValidMoves(self):
+         return self.getAllPossibleMoves()
+    
+
+    def getAllPossibleMoves(self):
+         moves = []
+         for r in range(len(self.board)):
+              for c in range(len(self.board[r])):
+                   turn = self.board[r][c][0]
+                   if(turn == 'w' and self.whitetoMove) or (turn == 'b' and not self.whitetoMove):
+                        piece = self.board[r][c][1]
+                        if piece == "p":
+                            print("here")
+                            return self.getPawnMoves(r,c,moves)
+                        elif piece == "r":
+                             return self.getRookMoves(r,c,moves)
+         return moves 
+
+    def getPawnmoves(self,r,c,moves):
+         if self.whitetoMove:
+              if self.board[r-1][c] == "--":
+                   moves.append(Move((r,c),(r-1,c),self.board))
+                   if r==6 and self.board[r-2][c] == "--":
+                       moves.append(Move((r,c),(r-2,c),self.board)) 
+
+
+    def getRookmoves(self,r,c,moves):
+         pass
+    
 
 class Move():
 
@@ -40,6 +79,14 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+        print(self.moveID)
+
+    def __eq__(self,other):
+         if isinstance(other,Move):
+              return self.moveID == other.moveID
+         return False
+
 
     def getChessNotation(self):
         return self.getRankFile(self.startRow,self.startCol) + self.getRankFile(self.endRow,self.endCol)
